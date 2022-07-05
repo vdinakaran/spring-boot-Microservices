e026e565ec4130a050b26779fc2ac326a840f6a3
varshini <varshini.dinakaran@capgemini.com>
2022-07-05 15:43:01 +0530

    files added

---
+++ limits-service/limits-service/src/main/java/com/javatpoint/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,16 @@
+package com.javatpoint.microservices.limitsservice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.javatpoint.microservices.limitsservice.bean.LimitConfiguration;
+@RestController
+public class LimitsConfigurationController 
+{
+@Autowired	
+private Configuration configuration;	
+@GetMapping("/limits")
+public LimitConfiguration retriveLimitsFromConfigurations()
+{
+return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+}
+}
